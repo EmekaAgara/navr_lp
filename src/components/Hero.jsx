4bbcdd4d122ca9872e6b2cc24cd003be5fe6ac7a
Emeka Agara <emekaagara@gmail.com>
2022-11-13 18:47:48 +0100

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
+
+const Hero = () => {
+  return (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center cursor-pointer py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">Google Maps</span> Platform Mobile App Project{" "}
+            <span className="text-white"></span>
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            Affordable<br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Boat Trips</span>{" "}
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          Charter Services.
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Navr is the best way to find and book boat trips in your Location. With features like getting estimated trip prices based on travel distance and boat type you're sure to find the perfect boat trip. You can also List your boat for daily payments.
+        </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,231 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "App",
+  },
+  {
+    id: "clients",
+    title: "Partner",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Book Boat Trips",
+    content:
+      "Navr is the best way to find and book boat trips in your area. With features like getting estimated trip prices based on travel distance and boat type",
+  },
+
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Charter Boats",
+    content:
+      "Search for the boat type and location you're looking for, and get a list of boats to Charter, you can either book your trip or find more information about each boat.",
+  },
+
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Partner with Us",
+    content:
+      "You can also list your own boat here so others can book trips on it, while you receive daily payment for trips completed by your boat",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Track a Boat",
+    content:
+      "Navr is finalizing the implementation of live location services for tracking boats and vessels in the ocean to estimate their exact location and ETA.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Search for the boat type and location you're looking for, and get a list of boats to rent.",
+    name: "Aisha W.M",
+    title: "Product Designer",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "We offer an all-inclusive price with no hidden fees — just the price you're quoted. So you can make sure that you're getting a fair deal before booking!",
+    name: "Emeka Agara",
+    title: "Designer & Developer",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Navr is the best way to find and book boat trips in your area. With features like getting estimated trip prices based on travel distance.",
+    name: "Joe Agara",
+    title: "Product Mangager",
+    img: people03,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Not only can you book a trip, but with our Boat Finder feature, track boats in real-time to see their exact location or estimated arrival time.",
+    name: "Mayowa",
+    title: "Developer",
+    img: people03,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Different things that people want to do on a boat. That's why NAVR makes it easy for everyone to find their perfect adventure.",
+    name: "Yan Zu",
+    title: "Developer",
+    img: people03,
+  },
+
+  {
+    id: "feedback-3",
+    content:
+      "Track boats in real-time to see their exact location or estimated arrival time. You can also list your own boat online so others can book trips on it as well!.",
+    name: "i Oracle",
+    title: "Developer",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "#",
+      },
+      {
+        name: "How it Works",
+        link: "#",
+      },
+      {
+        name: "Create",
+        link: "#",
+      },
+      {
+        name: "Explore",
+        link: "#",
+      },
+      {
+        name: "Terms & Services",
+        link: "#",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "#",
+      },
+      {
+        name: "Partners",
+        link: "#",
+      },
+      {
+        name: "Suggestions",
+        link: "#",
+      },
+      {
+        name: "Blog",
+        link: "#",
+      },
+      {
+        name: "Newsletters",
+        link: "#",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "#",
+      },
+      {
+        name: "Become a Partner",
+        link: "#",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "#",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "#",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "#",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "#",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
